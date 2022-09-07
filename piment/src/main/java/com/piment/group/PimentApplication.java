4d5cbedc24027f4cbc8d5fed6476a247c97d9cbd
fly-yeseul <87890093+fly-yeseul@users.noreply.github.com>
2022-09-07 14:47:10 +0900

    Add files via upload

---
+++ piment/src/main/java/com/piment/group/PimentApplication.java
@@ -0,0 +1,13 @@
+package com.piment.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PimentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PimentApplication.class, args);
+    }
+
+}
---
+++ piment/src/test/java/com/piment/group/PimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piment.group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PimentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
